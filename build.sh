d0542bdaa4080934b8cf6e4a0857d79a3520ce14
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2017-08-02 03:13:31 +0530

    build.sh: Add support to generate ECC for dev

--- build.sh
+++ build.sh
@@ -148,3 +148,12 @@ done
 mv $TMPFILE ima_catalog.bin
 rm -rf $TMPFILE
 
+if [ "$3" == "dev" ]; then
+    if [ "$2" == "POWER8" ]; then
+	$bs_value=32
+    else
+	$bs_value=256
+    fi
+$1/dd if=./ima_catalog.bin bs=${bs_value}K count=1 > ./ima_catalog.temp.bin
+$1/ecc --inject ./ima_catalog.temp.bin --output ./ima_catalog.bin.ecc --p8
+fi
